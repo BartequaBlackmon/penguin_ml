3801a9255a4df6a395e4815886c5bc370137d364
BartequaBlackmon <bartequa@gmail.com>
2024-04-09 07:42:31 -0400

    my first repos commit

--- penguins_ml.py
+++ penguins_ml.py
@@ -1,8 +1,12 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle
+import joblib
 penguin_df = pd.read_csv("C:/Users/black/Downloads/penguins.csv")
 print(penguin_df.head())
 penguin_df.dropna(inplace=True)
@@ -17,8 +21,15 @@ y_pred = rfc.predict(x_test.values)
 score = accuracy_score(y_pred, y_test)
 print('Our accuracy score for this model is {}'.format(score))
 rf_pickle = open('random_forest_penguin.pickle', 'wb')
-pickle.dump(rfc, rf_pickle)
+joblib.dump(rfc, rf_pickle)
 rf_pickle.close()
 output_pickle = open('output_penguin.pickle', 'wb')
-pickle.dump(uniques, output_pickle)
+joblib.dump(uniques, output_pickle)
 output_pickle.close()
+fig, ax = plt.subplots()
+ax = sns.barplot(x=rfc.feature_importances_, y=features.columns)
+plt.title('Which features are the most important for species prediction?')
+plt.xlabel('Importance')
+plt.ylabel('Feature')
+plt.tight_layout()
+fig.savefig('feature_importance.png')
